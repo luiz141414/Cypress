dcb8036cd67b24a87f2a32e387231d20d7f79056
luiz fernando silva <luiz_santa@hotmail.com>
2024-10-13 23:53:50 -0300

    teste6

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'seu-project-id-gerado',
   extends: ['plugin:cypress/recommended'],
   rules: {
     'no-unused-vars': ['error', { 'varsIgnorePattern': '^cy$' }]
